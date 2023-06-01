718adf7173727233fa958a0b97000d8edc2a0f88
Guilherme Akinyele de Almeida <74839828+gui-akinyele@users.noreply.github.com>
2023-06-01 01:02:10 -0300

    Atualização do exercicio

--- Modulo 8 - Exercicios/exercicio_27.js
+++ Modulo 8 - Exercicios/exercicio_27.js
@@ -3,3 +3,8 @@
 //* Crie um array com 2 elementos e outro com 4;
 //* Imprima o número de elementos de cada um na tela;
 
+let arr1 = [15,30];
+let arr2 = [5,7,8,9];
+
+console.log(arr1.length);
+console.log(arr2.length);
