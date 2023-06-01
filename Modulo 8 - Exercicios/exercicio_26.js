25c39f307970cc38351d7d6f69e808be845d73a7
Guilherme Akinyele de Almeida <74839828+gui-akinyele@users.noreply.github.com>
2023-06-01 00:42:51 -0300

    Add files via upload

---
+++ Modulo 8 - Exercicios/exercicio_26.js
@@ -0,0 +1,11 @@
+//! Exercicio 01
+
+//* Crie um array com 5 itens;
+//* Acesse o item 1,3,4;
+//! Obs: arrays começam no índice 0;
+
+let arr = [10,20,30,40,50];
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[3]);
---
+++ Modulo 8 - Exercicios/exercicio_27.js
@@ -0,0 +1,5 @@
+//! Exercicio 02
+
+//* Crie um array com 2 elementos e outro com 4;
+//* Imprima o número de elementos de cada um na tela;
+
