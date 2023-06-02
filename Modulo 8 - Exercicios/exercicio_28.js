091561800f38a501cd46ff95b3eb4ae5b34cefa8
Guilherme Akinyele de Almeida <74839828+gui-akinyele@users.noreply.github.com>
2023-06-01 23:24:50 -0300

    Upload do exercício 28

---
+++ Modulo 8 - Exercicios/exercicio_28.js
@@ -0,0 +1,15 @@
+//! Exercicio 03
+//* Crie um objeto onibus;
+//* Com as propriedades rodas = 8;
+//* Limite de passageiros = 40;
+//* Portas = 2;
+
+//! Imprima todas as propriedades no console;
+
+let onibus = {
+    rodas: 8,
+    limitePassageiros: 40,
+    portas: 2,
+}
+
+console.log(onibus);
