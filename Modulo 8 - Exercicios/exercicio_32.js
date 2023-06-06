0dec1b97d6aa930a5adc465224d293bc21cd1b34
Guilherme Akinyele de Almeida <74839828+gui-akinyele@users.noreply.github.com>
2023-06-05 22:58:40 -0300

    Upload Exercicio 32

---
+++ Modulo 8 - Exercicios/exercicio_32.js
@@ -0,0 +1,10 @@
+//! Exercicio 07
+
+//* Crie um array de 5 elementos;
+//* Faça uma iteração entre todos eles e imprima no console o valor;
+
+let arr = [1,2,3,4,5];
+
+for (let i = 0; i <= arr.length; i++){
+    console.log(arr[i]);
+}
