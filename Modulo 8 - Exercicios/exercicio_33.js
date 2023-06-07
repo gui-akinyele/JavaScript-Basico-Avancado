8a2f0a8bc58ef47d2231a3c04b4b2e41714cb063
Guilherme Akinyele de Almeida <74839828+gui-akinyele@users.noreply.github.com>
2023-06-07 00:13:59 -0300

    Upload Exercicio 33

---
+++ Modulo 8 - Exercicios/exercicio_33.js
@@ -0,0 +1,14 @@
+//! Exercicio 08
+
+//* Crie um Json com 3 propriedades;
+//* Atribua ele a uma variável;
+//* Acesse as propriedades imprimindo no console;
+
+let funcionario = {
+    "nome": "Guilherme",
+    "idade": 32,
+    "cargo": "Web Developer"
+}
+
+console.log(funcionario.nome);
+console.log(funcionario.cargo);
